89d66bd4c7fa59f4bd70c7e1bb11753252497426
gatoAstronomo <151584479+gatoAstronomo@users.noreply.github.com>
2024-03-21 09:42:54 -0300

    Agrega metodo mayorSismo e ingresar datos

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args){
@@ -11,4 +13,30 @@ public class Main {
         System.out.println("iv. Enviar SMS por cada sismo mayor o igual a 7.0");
         System.out.println("v.  Salir (S/N)");
     }
+
+    private static double[][] ingresarDatos(){
+        Random random = new Random();
+        double numeroAleatorio;
+        double[][] sismos = new double[7][24];
+
+        for(int i = 0; i < 7; i++) {
+            for (int j = 0; j < 24; j++) {
+                numeroAleatorio = random.nextDouble() * 9.9;
+                sismos[i][j] = numeroAleatorio;
+            }
+        }
+
+        return sismos;
+    }
+
+    private static double buscarMayorSismo(double[][] sismos) {
+        double mayor = -1;
+        for (int i = 0; i < 7; i++)
+            for (int j = 0; j < 24; j++)
+                if(sismos[i][j] > mayor)mayor = sismos[i][j];
+
+        return mayor;
+    }
+
+    //private static int contarSismos(double[][] sismos)
 }

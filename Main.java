4d77758ef8e890706641a6d13495f1a3ca511caf
gatoAstronomo <151584479+gatoAstronomo@users.noreply.github.com>
2024-03-21 10:43:45 -0300

    Cambiar el formato de las impresiones por pantalla

--- Main.java
+++ Main.java
@@ -38,11 +38,11 @@ public class Main {
     }
 
     private static void imprimirMenu(){
-        System.out.println("i. Ingresar datos");
-        System.out.println("ii. Mostrar sismo de mayor magnitud");
-        System.out.println("iii. Contar sismos mayores o iguales a 5.0");
-        System.out.println("iv. Enviar SMS por cada sismo mayor o igual a 7.0");
-        System.out.println("v.  Salir (S/N)");
+        System.out.println("1. Ingresar datos");
+        System.out.println("2. Mostrar sismo de mayor magnitud");
+        System.out.println("3. Contar sismos mayores o iguales a 5.0");
+        System.out.println("4. Enviar SMS por cada sismo mayor o igual a 7.0");
+        System.out.println("5. Salir (S/N)");
     }
 
     private static void ingresarDatos(){
